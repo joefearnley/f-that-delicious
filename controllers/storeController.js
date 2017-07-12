6416db4888b2c5e20e6b962874e46ba7dd103330
Joe Fearnley <joe.fearnley@gmail.com>
2017-07-12 20:56:34 +0000

    Learning about async/await

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   res.render('index');
 };
@@ -8,6 +11,8 @@ exports.addStore = (req, res) => {
   });
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully Create ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store}`);
 };
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const storeContoller = require('../controllers/storeController');
 
+const { catchErrors } = require('../handlers/errorHandlers')
+
 router.get('/', storeContoller.homePage);
 router.get('/add', storeContoller.addStore);
-router.post('/add', storeContoller.createStore);
+router.post('/add', catchErrors(storeContoller.createStore));
 
 module.exports = router;
