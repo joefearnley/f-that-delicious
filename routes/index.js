bbff8a3a61d04caabeb4f7ecbd618746c584cfe7
Joe Fearnley <joe.fearnley@gmail.com>
2017-08-08 20:14:44 +0000

    Setting up account editing and reset password.

--- routes/index.js
+++ routes/index.js
@@ -35,5 +35,13 @@ router.post('/register',
   authController.login
 );
 router.get('/logout', authController.logout);
+router.get('/account', userController.account);
+router.post('/account', userController.updateAccount);
+router.post('/account/forgot', catchErrors(authController.forgot));
+router.get('/account/reset/:token', catchErrors(authController.reset));
+router.post('/account/reset/:token',
+  authController.confirmedPasswords,
+  catchErrors(authController.update)
+);
 
 module.exports = router;
